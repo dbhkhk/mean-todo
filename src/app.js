1f06b5d0b2de4d990c0ac0e0135461ddc10e200e
Brody Chen <rufengch@gmail.com>
2016-11-06 21:14:34 -0800

    Add router

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ const app = express();
 
 app.use('/', express.static('public'));
 
+const router = express.Router();
+
+router.get('/todos', (req, res) => {
+  res.json({todos: []});
+});
+
+// TODO: Add POST route to create entries
+
+// TODO: Add PUT route to update existing entries
+
+// TODO: Add DELETE route to delete entries
+
+app.use('/api', router);
+
 app.listen(3000, () => {
   console.log('The server is running on port 3000.');
 });
