8d9494b559cb78f22fd1b948d4f83a2ddafb8687
Brody Chen <rufengch@gmail.com>
2016-11-09 18:10:03 -0800

    Add PUT route

--- src/api/index.js
+++ src/api/index.js
@@ -25,7 +25,19 @@ router.post('/todos', (req, res) => {
   });
 });
 
-// TODO: Add PUT route to update existing entries
+router.put('/todos/:id', (req, res) => {
+  const id = req.params.id,
+        todo = req.body;
+  if (todo && todo._id !== id) {
+    return res.status(500).json({err: "Ids don't match!"});
+  }
+  Todo.findByIdAndUpdate(id, todo, {new: true}, (err, todo) => {
+    if (err) {
+      return res.status(500).json({err: err.message});
+    }
+    res.json({todo, message: 'Todo updated'});
+  });
+});
 
 // TODO: Add DELETE route to delete entriesapi
 
